965e88c2b0b91a3d929ac372039b331d8b88eeb5
Jorge Rivera <jrivera@VESSELARG.local>
2023-01-19 16:29:51 -0300

    todo review add function

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
-console.log("app Working")
+console.log("app Working");
 
+let numero1 = 0;
+let numero2 = 0;
+let operation = 0;
 let resultado = 0;
-let operation = "";
 
-
-
-
-function operator(value) {
-    console.log("Se ha presionado:" + value);
-    resultado = value;
+function print(value) {
+  if (operation == 0) {
+    if (numero1 == 0) {
+      numero1 = value;
+      console.log("Primer Ingreso1: " + numero1);
+    } else {
+      numero1 += value;
+      console.log("Numero1: " + numero1);
+    }
+  } else {
+    if (numero2 == 0) {
+      numero2 = value;
+      console.log("Primer Ingreso2: " + numero2);
+    } else {
+      numero2 += value;
+      console.log("Numero2: " + numero2);
+    }
+  }
 }
 
-function reset() {
-    console.log("Presion Reset.")
-resultado += 0;
-operation = "";
-
+function operator(x, value) {
+  operation = 0;
+  console.log("Se ha presionado: " + value);
+  operation = x;
 }
 
 function result() {
-    console.log("Presion Result." + resultado)
+  switch (operation) {
+    case 1:
+      resultado = numero1 - numero2;
+      break;
+    case 2:
+      resultado = numero1 + numero2;
+      break;
+    case 3:
+      resultado = numero1 / numero2;
+      break;
+    case 4:
+      resultado = numero1 * numero2;
+      break;
+  }
 
+  console.log("Resultado: " + resultado);
 }
-    
-    function print(value) {
-        console.log("Se ha presionado:" + value);
-        resultado = resultado + operation + value;
-    }
+
+function reset() {
+  console.log("Presion Reset.");
+  numero1 = 0;
+  numero2 = 0;
+  operation = 0;
+  resultado = 0;
+}
